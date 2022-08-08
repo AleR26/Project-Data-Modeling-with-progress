fb311995e159b71e58b68dc842a0b250fcac0579
Ale <rivassiale@gmail.com>
2022-08-08 16:19:53 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Project-Data-Modeling-with-progress
+# Project: Data Modeling with Postgres
+
+In this project we work with a JSON files provided by startup called Sparkify, we create a ETL pipeline for that first:
+1. Create a database with 5 tables.
+2. Extract information from JSON files.
+3. Use pandas library to transform data
+4. Insert the data in SQL tables
+
+You can see how this project works if you runs the python scripts for that is necessary:
+- Open the terminal
+- Run the create_tables.py file to create the database and the tables for that is necessary write the next statement:
+    - python create_tables.py
+- Run etl.py file in this file we connect to the database and insert the information that we need in our database, for that is necessary write the next statement:
+    - python etl.py
+    
+
+## Files in the repository:
+- data folder: Contains the JSON files 
+- create_tables.py: In this file you are going to find the functions to: create the database, create tables and drop tables.
+- sql_querys.py: This file contains every query and sql statement to: drop tables, create tables, insert data, and select data.
+- elt.ipynb: This is the jupyter notebook where contains the ETL process for each of the tables we can run step by step in the notebook!
+- etl.py: Function for the ETL process in this project.
+- README.md: this document containts the information for the project.
+- test.ipybn: Jupyther notebook to check the database.
+
+State and justify your database schema design and ETL pipeline.
+## Schema design
+I use a star schema for the database because we need 4 dimension tables and 1 fact table to store the data. With this schema is time efficient and allow us to query data and create joins more easily.
+
+## ETL pipeline
+Steps in the ETL pipeline:
+1. First we read the song_data JSON file then insert the data in song and artist tables.
+2. Convert the log_data JSON file in a dataframe
+3. Transforme data and insert into time and users tables
+4. Finally we create a join to insert into songplay fact table
+
+
+
+
+
---
+++ sql_queries.py
@@ -0,0 +1,40 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
+
+# CREATE TABLES
+
+songplay_table_create = ("CREATE table IF NOT EXISTS songplays (songplay_id SERIAL PRIMARY KEY, start_time timestamp NOT NULL,user_id int NOT NULL,level varchar(50),song_id varchar(50),artist_id varchar(50),session_id int,location varchar(250),user_agent varchar(250))")
+
+user_table_create = ("CREATE TABLE IF NOT EXISTS users (user_id int PRIMARY KEY, first_name varchar(50), last_name varchar(50), gender varchar(50), level varchar(50))")
+
+song_table_create = ("CREATE TABLE IF NOT EXISTS songs(song_id varchar(50) PRIMARY KEY, title varchar(250) NOT NULL,artist_id varchar(50), year int, duration float NOT NULL)")
+
+artist_table_create = ("CREATE TABLE IF NOT EXISTS artists (artist_id TEXT PRIMARY KEY, name TEXT NOT NULL, location TEXT, latitude float, longitud float)")
+
+time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time time PRIMARY KEY, hour int, day int, week int, month int, year int, weekday int)")
+
+# INSERT RECORDS
+ 
+songplay_table_insert = ("INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) values (%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING")
+
+user_table_insert = ("INSERT INTO users (user_id,first_name,last_name,gender,level) values(%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING")
+
+song_table_insert = ("INSERT INTO songs(song_id, title, artist_id, year, duration) values (%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING")
+
+artist_table_insert = ("INSERT INTO artists(artist_id, name, location, latitude, longitud) values (%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING")
+
+
+time_table_insert = ("INSERT INTO time(start_time,hour,day,week,month,year,weekday) values(%s,%s,%s,%s,%s,%s,%s) ON CONFLICT DO NOTHING")
+
+# FIND SONGS
+
+song_select = ("SELECT song_id,artists.artist_id FROM artists join songs on songs.artist_id = artists.artist_id WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s")
+
+# QUERY LISTS
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
